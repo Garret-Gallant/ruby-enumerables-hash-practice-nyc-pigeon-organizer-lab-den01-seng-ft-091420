e19e5e9de14c83334d78252eff55be92c14b4bb5
Garret Gallant <gallanttechnological@gmail.com>
2020-09-04 20:22:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash.each_with_object({}) do |(keys, value), array| 
+    value.each do |inner_key, names|
+      names.each do |name|
+      if !array[name]
+        array[name] = {}
+      end
+    end
+  end
+    array
+end
+
 end
